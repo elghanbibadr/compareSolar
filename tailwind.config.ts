a9392ae21d299607b0c88d3936426ae2227a21e2
elghanbibadr <bghanbi50@gmail.com>
2025-01-13 15:58:00 +0100

    add the hero background image

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,9 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        backgroundImage: {
+          'hero-pattern': "url('/public/hero.jpg')",
+        },
       },
     },
   },
