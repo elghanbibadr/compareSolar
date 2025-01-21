fde7287bb447804bcef8780d2731a6e3ee771e46
elghanbibadr <bghanbi50@gmail.com>
2025-01-21 14:16:32 +0100

    fix prod issue

--- app/componenets/Footer.tsx
+++ app/componenets/Footer.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import React from 'react'
-import sun from "@/public/images/sun.png";
 import logo from "@/public/images/SolarLocal Logo Files/SolarLocal Logo_coloured.svg";
 
 const Footer = () => {
